e9c40d933b9a29bd33d8c8ede099360c6e541313
mnubhan <nubhant@gmail.com>
2024-09-17 22:45:48 +0100

    feat:improve BMI measurement toggle to clear inputs and remove alerts - Clear all input fields when the BMI measurement system is toggled. - Remove any alert classes and messages from input fields. - Ensure a smooth user experience by resetting the form state on measurement change.

--- assets/js/toggleMeasurementSystem.js
+++ assets/js/toggleMeasurementSystem.js
@@ -1,27 +1,33 @@
-export function toggleMeasurementSystem(
-  typeMeasurement,
-) {
+import removeAlertInput from "./removeAlertInput.js";
+import removeAlertMessage from "./removeAlertMessage.js";
+
+export function toggleMeasurementSystem(typeMeasurement) {
   let weightSt = document.querySelector(".weight__st");
   let weightLb = document.querySelector(".weight__lb");
   let weightKg = document.querySelector(".weight__kg");
   let heightCm = document.querySelector(".height__cm");
   let heightFt = document.querySelector(".height__ft");
   let heightIn = document.querySelector(".height__in");
-  if (typeMeasurement==="metric") {
-    weightSt.setAttribute("hidden", true);
-    weightLb.setAttribute("hidden", true);
-    heightFt.setAttribute("hidden", true);
-    heightIn.setAttribute("hidden", true);
-
-    weightKg.removeAttribute("hidden");
-    heightCm.removeAttribute("hidden");
+  let imperialParams = [heightFt, heightIn, weightSt, weightLb];
+  let metricParams = [weightKg, heightCm];
+  if (typeMeasurement === "metric") {
+    imperialParams.forEach((element) => {
+      element.setAttribute("hidden", true);
+    });
+    metricParams.forEach((element) => {
+      element.removeAttribute("hidden");
+    });
   } else {
-    weightSt.removeAttribute("hidden");
-    weightLb.removeAttribute("hidden");
-    heightFt.removeAttribute("hidden");
-    heightIn.removeAttribute("hidden");
-
-    weightKg.setAttribute("hidden", true);
-    heightCm.setAttribute("hidden", true);
+    imperialParams.forEach((element) => {
+      element.removeAttribute("hidden");
+    });
+    metricParams.forEach((element) => {
+      element.setAttribute("hidden", true);
+    });
+    metricParams.concat(imperialParams).forEach((element) => {
+      element.children[0].value = "";
+      removeAlertInput(element.children[0]);
+      removeAlertMessage(element.children[0]);
+    });
   }
 }
