32792097c6ba533eb5b43eaa223aefea2eebceb9
mnubhan <nubhant@gmail.com>
2024-09-18 00:18:56 +0100

    feat:implement ideal weight range for user

--- assets/js/calculateBmiImperial.js
+++ assets/js/calculateBmiImperial.js
@@ -1,28 +1,50 @@
-import {emptyInput} from "./emptyInput.js"
+import { emptyInput } from "./emptyInput.js";
 import { addAlertMessage } from "./addAlertMessage.js";
-import alertInput from "./alertInput.js"
+import alertInput from "./alertInput.js";
 const bmiValueElement = document.querySelector(".bmi__value");
-const calculateBmiImperial = (inches,ft,lb,st)=>{
-    if((inches.value === "" && ft.value === "" )|| (lb.value === "" && st.value === "")){
-        if(lb.value === "" && st.value === ""){
-            let weightInputDiv = document.querySelector(".bmi__input-weight");
-            addAlertMessage(weightInputDiv);
-            [lb,st].forEach((element)=>{alertInput(element)})
-        }
-        if(inches.value === "" && ft.value === "" ){
-            let heightInputDiv = document.querySelector(".bmi__input-height");
-            addAlertMessage(heightInputDiv);
-            [inches,ft].forEach((element)=>{alertInput(element)})
-        }
-       
+const weightHealthyRange = document.querySelector(".weight__healthy-range");
+const calculateBmiImperial = (inches, ft, lb, st) => {
+  if (
+    (inches.value === "" && ft.value === "") ||
+    (lb.value === "" && st.value === "")
+  ) {
+    if (lb.value === "" && st.value === "") {
+      let weightInputDiv = document.querySelector(".bmi__input-weight");
+      addAlertMessage(weightInputDiv);
+      [lb, st].forEach((element) => {
+        alertInput(element);
+      });
     }
-    else{
-        let heightIn= inches.value + ft.value * 12;
-        let weightLb = (lb.value + st.value * 14)*703;
-        let bmiResult = (weightLb / Math.pow(heightIn,2)).toFixed(2);
-        emptyInput([inches,ft,lb,st]);
-        bmiValueElement.textContent= bmiResult;
+    if (inches.value === "" && ft.value === "") {
+      let heightInputDiv = document.querySelector(".bmi__input-height");
+      addAlertMessage(heightInputDiv);
+      [inches, ft].forEach((element) => {
+        alertInput(element);
+      });
     }
-}
+  } else {
+    let heightIn = parseInt(inches.value) + ft.value * 12;
+    let weightLb = parseInt(lb.value) + parseInt(st.value) * 14;
+    let bmiResult = ((weightLb / Math.pow(heightIn, 2)) * 703).toFixed(2);
+    console.log("height In", inches.value);
+    console.log("height Ft", ft.value * 12);
+    console.log("weight Lb", weightLb);
+    console.log("height In", heightIn);
+    let minimumIdealWeight = ((18.5 * Math.pow(heightIn, 2)) / 703).toFixed(2);
+    let maximumIdealWeight = ((24.9 * Math.pow(heightIn, 2)) / 703).toFixed(2);
+    let minimumIdealWeightSt = Math.floor(minimumIdealWeight / 14);
+    let minimumIdealWeightLb = Math.floor(
+      (minimumIdealWeight / 14 - Math.floor(minimumIdealWeight / 14)) * 14
+    );
+    let maximumIdealWeightSt = Math.floor(maximumIdealWeight / 14);
+    let maximumIdealWeightLb = Math.floor(
+      (maximumIdealWeight / 14 - Math.floor(maximumIdealWeight / 14)) * 14
+    );
+    let idealWeightRange = `${minimumIdealWeightSt}st ${minimumIdealWeightLb}lb - ${maximumIdealWeightSt}st ${maximumIdealWeightLb}lb`;
+    bmiValueElement.textContent = bmiResult;
+    weightHealthyRange.textContent = idealWeightRange;
+    emptyInput([inches, ft, lb, st]);
+  }
+};
 
-export {calculateBmiImperial};
+export { calculateBmiImperial };
--- assets/js/calculateBmiMetric.js
+++ assets/js/calculateBmiMetric.js
@@ -2,6 +2,7 @@ import { emptyInput } from "./emptyInput.js";
 import { addAlertMessage } from "./addAlertMessage.js";
 import alertInput from "./alertInput.js";
 const bmiValueElement = document.querySelector(".bmi__value");
+const weightHealthyRange = document.querySelector(".weight__healthy-range");
 const calculateBmiMetric = (weight, height) => {
   if (weight.value === "" || height.value === "") {
     if (weight.value === "") {
@@ -9,7 +10,6 @@ const calculateBmiMetric = (weight, height) => {
       addAlertMessage(weightInputDiv);
       alertInput(weight);
     }
-
     if (height.value === "") {
       let heightInputDiv = document.querySelector(".bmi__input-height");
       addAlertMessage(heightInputDiv);
@@ -17,8 +17,12 @@ const calculateBmiMetric = (weight, height) => {
     }
   } else {
     let bmiResult = (weight.value / Math.pow(height.value / 100, 2)).toFixed(2);
-    emptyInput([weight, height]);
+    let minimumIdealWeight = (18.5 * Math.pow(height.value / 100, 2)).toFixed(2);
+    let maximumIdealWeight = (24.9 * Math.pow(height.value / 100, 2)).toFixed(2);
+    let idealWeightRange = `${minimumIdealWeight}kgs - ${maximumIdealWeight}kgs`;
     bmiValueElement.textContent = bmiResult;
+    weightHealthyRange.textContent = idealWeightRange;
+    emptyInput([weight, height]);
   }
 };
 
